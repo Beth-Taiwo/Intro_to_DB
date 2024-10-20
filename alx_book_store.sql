dcf10f62dd650a729e23a90af5232910b83ae52a
Beth Taiwo <popoolataiwo22@gmail.com>
2024-10-20 23:01:48 +0000

    changed code syntax from python to sql

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -1,61 +1,40 @@
-import mysql.connector
+CREATE DATABASE IF NOT EXISTS alx_book_store;
 
-mydb = mysql.connector.connect(
-    host="localhost",
-    user="yourusername",
-    password="yourpassword",
-)
+USE alx_book_store;
 
-mycursor = mydb.cursor()
-
-mycursor.execute("CREATE DATABASE IF NOT EXISTS alx_book_store")
-
-mydb.database = 'alx_book_store'
-
-mycursor.execute("""
 CREATE TABLE IF NOT EXISTS Authors(
     author_id INT AUTO_INCREMENT PRIMARY KEY,
     author_name VARCHAR(215)
 )
-""")
 
-mycursor.execute(""" 
 CREATE TABLE IF NOT EXISTS Books(
     book_id INT AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(130),
     author_id INT,
-    FOREIGN KEY (author_id) REFERENCES Authors(author_id),
     price DOUBLE,
-    publication_date DATE)
-""")
+    publication_date DATE,
+    FOREIGN KEY (author_id) REFERENCES Authors(author_id)
+)
 
-mycursor.execute("""
 CREATE TABLE IF NOT EXISTS Customers(
     customer_id INT AUTO_INCREMENT PRIMARY KEY,
     customer_name VARCHAR(215),
     email VARCHAR(215),
     address TEXT
 )
-""")
 
-mycursor.execute("""
 CREATE TABLE IF NOT EXISTS Orders(
     order_id INT AUTO_INCREMENT PRIMARY KEY,
     customer_id INT,
-    FOREIGN KEY (customer_id) REFERENCES Customers(customer_id),
     order_date DATE
+    FOREIGN KEY (customer_id) REFERENCES Customers(customer_id),
 )
-""")
 
-mycursor.execute("""
 CREATE TABLE IF NOT EXISTS Order_Details(
     order_detail_id INT AUTO_INCREMENT PRIMARY KEY,
     order_id INT,
-    FOREIGN KEY (order_id) REFERENCES Orders(order_id),
     book_id INT,
-    FOREIGN KEY (book_id) REFERENCES Books(book_id),
     quantity DOUBLE
+    FOREIGN KEY (order_id) REFERENCES Orders(order_id),
+    FOREIGN KEY (book_id) REFERENCES Books(book_id),
 )
-""")
-
-print("Tables created successfully.")
