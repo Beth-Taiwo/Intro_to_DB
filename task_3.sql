06b039cfd33b38b79e98949a6fa1fb7a3baf17ea
Beth Taiwo <popoolataiwo22@gmail.com>
2024-10-20 22:05:19 +0000

    convert code syntax from python to sql

--- task_3.sql
+++ task_3.sql
@@ -1,21 +1,3 @@
-import mysql.connector
+use alx_book_store;
 
-mydb = mysql.connector.connect(
-    host="localhost",
-    user="yourusername",
-    password="yourpassword",
-)
-
-mycursor = mydb.cursor()
-
-mycursor.execute("CREATE DATABASE IF NOT EXISTS alx_book_store")
-    
-mycursor.execute("USE alx_book_store")
-
-mycursor.execute("SHOW TABLES")
-
-tables = mycursor.fetchall()
-
-print("List of tables in the database:")
-for table in tables:
-    print(table[0])
+SHOW TABLES;
