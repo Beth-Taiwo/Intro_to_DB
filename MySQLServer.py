cdf0b13396ac137e864aac5c565da4233678a7af
Beth Taiwo <popoolataiwo22@gmail.com>
2024-10-20 21:22:47 +0000

    updated exception statement

--- MySQLServer.py
+++ MySQLServer.py
@@ -1,5 +1,4 @@
 import mysql.connector
-from mysql.connector import Error
 
 try:
     mydb = mysql.connector.connect(
@@ -13,8 +12,8 @@ try:
     mycursor.execute("CREATE DATABASE IF NOT EXISTS alx_book_store")
     print("Database 'alx_book_store' created successfully!")
 
-except Error as e:
-    print(f"The error '{e}' occurred")
+except mysql.connector.Error:
+    print(f"An error has occurred")
 
 finally:
     mycursor.close()
